f46ae970fc849c17a5e4206cbcfcfca241588426
Warwick Stone <uozu.aho@gmail.com>
2013-11-20 22:19:32 +1100

    shortened dodo.py

--- dodo.py
+++ dodo.py
@@ -20,57 +20,33 @@ SOURCE_DIRS = [
     os.path.join(ARDUINO_ENV.libs_path, 'Adafruit_NeoPixel')
 ]
 
+INCLUDE_DIRS = [
+    'src',
+    os.path.join(ARDUINO_ENV.libs_path, 'Adafruit_NeoPixel')
+]
+
 
 # ---------------------------------------------------------------------
 # data
 
 class BuildData:
+    def __init__(self):
+        self.c_sources = []
+        self.cpp_sources = []
+        for path in SOURCE_DIRS:
+            self.c_sources += file_utils.find(path, '*.c')
+            self.cpp_sources += file_utils.find(path, '*.cpp')
 
-    """ This data is generated as doit tasks are generated """
+        self.objs = [self.get_obj_path(x) for x in self.c_sources + self.cpp_sources]
+        self.objs += [ARDUINO_ENV.core_lib_output_path]
 
-    c_sources = None
-    cpp_sources = None
-    objs = None
-    deps = None
+        self.deps = gcc_utils.get_dependency_dict(BUILD_DIR)
 
-    def get_c_sources(self):
-        if self.c_sources is None:
-            self.c_sources = []
-            for path in SOURCE_DIRS:
-                self.c_sources += file_utils.find(path, '*.c')
-        return self.c_sources
+    def get_obj_path(self, source_path):
+        return os.path.join(BUILD_DIR, source_path + '.o')
 
-    def get_cpp_sources(self):
-        if self.cpp_sources is None:
-            self.cpp_sources = []
-            for path in SOURCE_DIRS:
-                self.cpp_sources += file_utils.find(path, ['*.cpp', '*.ino'])
-        return self.cpp_sources
-
-    def get_objs(self):
-        if self.objs is None:
-            sources = self.get_c_sources() + self.get_cpp_sources()
-            self.objs = [get_obj_path(x) for x in sources]
-            self.objs += [ARDUINO_ENV.core_lib_output_path]
-        return self.objs
-
-    def get_deps(self):
-        if self.deps is None:
-            if os.path.exists(BUILD_DIR):
-                self.deps = gcc_utils.get_dependency_dict(BUILD_DIR)
-            else:
-                self.deps = []
-        return self.deps
-
-    def __str__(self):
-        output = 'c source files:\n'
-        output += ''.join('    ' + x + '\n' for x in self.get_c_sources())
-        output += '\nc++ source files:\n'
-        output += ''.join('    ' + x + '\n' for x in self.get_cpp_sources())
-        output += '\nfound dependency files for the following:\n'
-        output += ''.join('    ' + x + '\n' for x in self.get_deps())
-        return output
-
+    def get_dep_path(self, source_path):
+        return os.path.join(BUILD_DIR, source_path + '.d')
 
 build_data = BuildData()
 
@@ -78,30 +54,28 @@ build_data = BuildData()
 # ---------------------------------------------------------------------
 # utilities
 
-def get_obj_path(source_path):
-    return os.path.join(BUILD_DIR, source_path + '.o')
-
-
-def get_dep_path(source_path):
-    return os.path.join(BUILD_DIR, source_path + '.d')
-
-
 def get_source_dependencies(source):
-    deps = build_data.get_deps()
-    obj = get_obj_path(source)
-    if obj in deps:
-        dependencies = deps[obj]
+    obj = build_data.get_obj_path(source)
+    if obj in build_data.deps:
+        return build_data.deps[obj]
     else:
-        dependencies = [source]
-    return dependencies
+        return [source]
 
 
 def get_c_compile_command_str(source, obj):
     return gcc_utils.get_compile_cmd_str(source, obj,
                                          compiler=ARDUINO_ENV.c_compiler,
                                          defs=ARDUINO_ENV.cdefs,
-                                         includes=ARDUINO_ENV.cincludes,
+                                         includes=ARDUINO_ENV.cincludes + INCLUDE_DIRS,
                                          flags=ARDUINO_ENV.cflags)
+
+
+def get_cpp_compile_command_str(source, obj):
+    return gcc_utils.get_compile_cmd_str(source, obj,
+                                         compiler=ARDUINO_ENV.cpp_compiler,
+                                         defs=ARDUINO_ENV.cppdefs,
+                                         includes=ARDUINO_ENV.cppincludes + INCLUDE_DIRS,
+                                         flags=ARDUINO_ENV.cppflags)
 
 
 # ---------------------------------------------------------------------
@@ -113,9 +87,9 @@ def task_build_arduino_core():
 
 
 def task_compile_c():
-    for source in build_data.get_c_sources():
-        obj = get_obj_path(source)
-        dep = get_dep_path(source)
+    for source in build_data.c_sources:
+        obj = build_data.get_obj_path(source)
+        dep = build_data.get_dep_path(source)
         yield {
             'name': source + '.o',
             'actions': [(create_folder, [os.path.dirname(obj)]),
@@ -127,9 +101,9 @@ def task_compile_c():
 
 
 def task_compile_cpp():
-    for source in build_data.get_cpp_sources():
-        obj = get_obj_path(source)
-        dep = get_dep_path(source)
+    for source in build_data.cpp_sources:
+        obj = build_data.get_obj_path(source)
+        dep = build_data.get_dep_path(source)
         yield {
             'name': source + '.o',
             'actions': [(create_folder, [os.path.dirname(obj)]),
